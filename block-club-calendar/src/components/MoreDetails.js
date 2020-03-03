a2d51bdf2cd45c68bb644d61b596390341759ec4
Amanda Hogan <ahogan@teamworks.com>
2020-03-03 12:07:57 -0500

    Change more details

--- block-club-calendar/src/components/MoreDetails.js
+++ block-club-calendar/src/components/MoreDetails.js
@@ -1,34 +1,20 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React from 'react';
+import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const MoreDetails = (props) => {
-  const [modal, setModal] = useState(false);
-
-  const toggle = (event) => {
-    setModal(!modal);
-    props.history.push(`/api/events/${event.id}`);
-  };
-
   return (
     <div>
-      <nav>
-        <Link to="/">Login</Link>
-        <Link to="/api/events">Upcoming Events</Link>
-        <Link to="/api/users/events">My Events</Link>
-        <Link to="/api/create-event">Create Event</Link>
-      </nav>
-
-      <Modal key={props.event.id} isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>{props.event.eventTitle}</ModalHeader>
-        <ModalBody>{props.event.eventDescription}</ModalBody>
-        <ModalBody>{props.event.eventAddress}</ModalBody>
-        <ModalFooter>
+      <div key={props.event.id}>
+        <h3>{props.event.eventTitle}</h3>
+        <p>{props.event.eventDescription}</p>
+        <p>{props.event.eventAddress}</p>
+        <div>
           <Button color="primary" onClick={() => props.saveEvent(props.event)}>Confirm</Button>{' '}
           <Button color="primary" onClick={() => props.updateEvent(props.event.id)}>Update</Button>{' '}
           <Button color="secondary" onClick={() => props.deleteEvent(props.event.id)}>Delete</Button>
-        </ModalFooter>
-      </Modal>
+        </div>
+      </div>
     </div>
   );
 };
